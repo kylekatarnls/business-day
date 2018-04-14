1c78d3533db58572c554380d626cccdc623b9b87
KyleK <kylekatarnls@gmail.com>
2018-04-14 19:04:17 +0200

    Add unit tests for plural

--- tests/Cmixin/BusinessDayTest.php
+++ tests/Cmixin/BusinessDayTest.php
@@ -301,6 +301,12 @@ class BusinessDayTest extends TestCase
         self::assertSame('16/04/2018', Carbon::parse('2018-04-15 12:00:00')->addBusinessDays()->format('d/m/Y'));
         self::assertSame('17/04/2018', Carbon::parse('2018-04-16 12:00:00')->addBusinessDays()->format('d/m/Y'));
         self::assertSame('12/11/2018', Carbon::parse('2018-11-11 12:00:00')->addBusinessDays()->format('d/m/Y'));
+        self::assertSame('10/05/2018', Carbon::parse('2018-05-01 12:00:00')->addBusinessDays(6)->format('d/m/Y'));
+        self::assertSame('17/04/2018', Carbon::parse('2018-04-04 12:00:00')->addBusinessDays(9)->format('d/m/Y'));
+        self::assertSame('24/05/2018', Carbon::parse('2018-04-14 12:00:00')->addBusinessDays(25)->format('d/m/Y'));
+        self::assertSame('18/04/2018', Carbon::parse('2018-04-15 12:00:00')->addBusinessDays(3)->format('d/m/Y'));
+        self::assertSame('18/04/2018', Carbon::parse('2018-04-16 12:00:00')->addBusinessDays(2)->format('d/m/Y'));
+        self::assertSame('04/12/2018', Carbon::parse('2018-11-11 12:00:00')->addBusinessDays(17)->format('d/m/Y'));
     }
 
     public function testAddBusinessDayStatic()
@@ -347,6 +353,12 @@ class BusinessDayTest extends TestCase
         self::assertSame('13/04/2018', Carbon::parse('2018-04-15 12:00:00')->subBusinessDays()->format('d/m/Y'));
         self::assertSame('13/04/2018', Carbon::parse('2018-04-16 12:00:00')->subBusinessDays()->format('d/m/Y'));
         self::assertSame('09/11/2018', Carbon::parse('2018-11-11 12:00:00')->subBusinessDays()->format('d/m/Y'));
+        self::assertSame('23/04/2018', Carbon::parse('2018-05-01 12:00:00')->subBusinessDays(6)->format('d/m/Y'));
+        self::assertSame('21/03/2018', Carbon::parse('2018-04-04 12:00:00')->subBusinessDays(9)->format('d/m/Y'));
+        self::assertSame('09/03/2018', Carbon::parse('2018-04-14 12:00:00')->subBusinessDays(25)->format('d/m/Y'));
+        self::assertSame('11/04/2018', Carbon::parse('2018-04-15 12:00:00')->subBusinessDays(3)->format('d/m/Y'));
+        self::assertSame('12/04/2018', Carbon::parse('2018-04-16 12:00:00')->subBusinessDays(2)->format('d/m/Y'));
+        self::assertSame('17/10/2018', Carbon::parse('2018-11-11 12:00:00')->subBusinessDays(17)->format('d/m/Y'));
     }
 
     public function testSubBusinessDayStatic()
