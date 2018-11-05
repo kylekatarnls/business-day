e39f082c4bb2846fb07a31576ff0c91952df79ed
KyleKatarn <kylekatarnls@gmail.com>
2018-11-05 13:35:45 +0100

    Implement unpackHoliday and addHoliday methods

--- src/Cmixin/BusinessDay/HolidaysList.php
+++ src/Cmixin/BusinessDay/HolidaysList.php
@@ -109,65 +109,120 @@ class HolidaysList extends MixinBase
      *
      * @return \Closure
      */
+    public function pushHoliday()
+    {
+        $mixin = $this;
+
+        return function ($region, $holiday, $key = null) use ($mixin) {
+            $mixin->initializeHolidaysRegion($region);
+
+            if (is_string($key)) {
+                $mixin->holidays[$region][$key] = $holiday;
+
+                return isset($this) ? $this : null;
+            }
+
+            $mixin->holidays[$region][] = $holiday;
+
+            return isset($this) ? $this : null;
+        };
+    }
+
+    /**
+     * Add holidays to the holidays list.
+     *
+     * @return \Closure
+     */
+    public function addHoliday()
+    {
+        $mixin = $this;
+
+        return function ($region, $holiday, $key = null, $name = null, $observed = null) use ($mixin) {
+            static $dictionary, $observer;
+
+            $mixin->initializeHolidaysRegion($region);
+            $push = $mixin->pushHoliday();
+            $push($region, $holiday, $key);
+
+            if (isset($name) && $mixin instanceof Holiday) {
+                if (!isset($dictionary)) {
+                    $dictionary = $mixin->getHolidayNamesDictionary();
+                }
+                foreach ($name as $language => $text) {
+                    $dictionary($language);
+                    $mixin->holidayNames[$language][$key] = $text;
+                }
+            }
+
+            if (isset($observed) && $mixin instanceof HolidayObserver) {
+                if (!isset($observer)) {
+                    $observer = $mixin->setHolidayObserveStatus();
+                }
+                $observer($key, $observed);
+            }
+
+            return isset($this) ? $this : null;
+        };
+    }
+
+    /**
+     * Add holidays to the holidays list.
+     *
+     * @return \Closure
+     */
+    public function unpackHoliday()
+    {
+        $mixin = $this;
+
+        return function (&$holiday, &$name = null, &$observed = null) use ($mixin) {
+            if (!isset($holiday['date'])) {
+                throw new \InvalidArgumentException(
+                    'Holiday array definition should at least contains a "date" entry.'
+                );
+            }
+
+            if (isset($holiday['name'])) {
+                $name = $holiday['name'];
+            }
+
+            if (isset($holiday['observed'])) {
+                $observed = $holiday['observed'];
+            }
+
+            $holiday = $holiday['date'];
+
+            return $holiday;
+        };
+    }
+
+    /**
+     * Add holidays to the holidays list.
+     *
+     * @return \Closure
+     */
     public function addHolidays()
     {
         $mixin = $this;
 
         return function ($region, $holidays) use ($mixin) {
             $mixin->initializeHolidaysRegion($region);
-            $dictionary = null;
-            $observer = null;
+            $add = $mixin->addHoliday();
+            $unpack = $mixin->unpackHoliday();
 
             foreach ($holidays as $key => $holiday) {
                 $name = null;
                 $observed = null;
                 if (is_array($holiday)) {
-                    if (!isset($holiday['date'])) {
-                        throw new \InvalidArgumentException(
-                            'Holiday array definition should at least contains a "date" entry.'
-                        );
-                    }
-
                     if (is_int($key)) {
                         throw new \InvalidArgumentException(
                             'Holiday array definition need a string identifier as main array key.'
                         );
                     }
 
-                    if (isset($holiday['name'])) {
-                        $name = $holiday['name'];
-                    }
-
-                    if (isset($holiday['observed'])) {
-                        $observed = $holiday['observed'];
-                    }
-
-                    $holiday = $holiday['date'];
+                    $unpack($holiday, $name, $observed);
                 }
 
-                if (is_int($key)) {
-                    $mixin->holidays[$region][] = $holiday;
-                } else {
-                    $mixin->holidays[$region][$key] = $holiday;
-                }
-
-                if (isset($name) && $mixin instanceof Holiday) {
-                    if (!isset($dictionary)) {
-                        $dictionary = $mixin->getHolidayNamesDictionary();
-                    }
-                    foreach ($name as $language => $text) {
-                        $dictionary = $mixin->getHolidayNamesDictionary();
-                        $dictionary($language);
-                        $mixin->holidayNames[$language][$key] = $text;
-                    }
-                }
-
-                if (isset($observed) && $mixin instanceof HolidayObserver) {
-                    if (!isset($observer)) {
-                        $observer = $mixin->setHolidayObserveStatus();
-                    }
-                    $observer($key, $observed);
-                }
+                $add($region, $holiday, $key, $name, $observed);
             }
         };
     }
