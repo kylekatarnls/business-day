c771193024f0476ee320bee1c74d50859780b66e
Kyle <kylekatarnls@gmail.com>
2019-03-01 07:53:01 +0000

    Apply fixes from StyleCI

--- src/Cmixin/Holidays/fr-east.php
+++ src/Cmixin/Holidays/fr-east.php
@@ -1,6 +1,6 @@
 <?php
 
-return array_merge(include __DIR__ . '/fr-national.php', array(
+return array_merge(include __DIR__.'/fr-national.php', array(
     // Vendredi Saint
     'good-friday'        => '= easter - 2',
     // Saint Etienne
