a2d210cbf52c323c0b1217e9948cb09a763bb366
KyleK <kylekatarnls@gmail.com>
2019-03-16 22:56:41 +0100

    Fix code style

--- src/Cmixin/Holidays/ae-national.php
+++ src/Cmixin/Holidays/ae-national.php
@@ -1,12 +1,16 @@
 <?php
 
 return array(
-    'new-year'             => '01-01',
-    '12-02'                => '12-02',
-    '1-muharram'           => '= 1 Muharram',
-    '1-shawwal-p3d'        => '= 1 Shawwal P3D',
-    '10-dhu-al-hijjah-p3d' => '= 10 Dhu al-Hijjah P3D',
-    '27-rajab'             => '= 27 Rajab',
-    '1-ramadan'            => '= 1 Ramadan',
-    '12-rabi-al-awwal'     => '= 12 Rabi al-awwal',
+    'new-year'            => '01-01',
+    '12-02'               => '12-02',
+    '1-muharram'          => '= 1 Muharram',
+    '1-shawwal'           => '= 1 Shawwal',
+    '1-shawwal-p1'        => '= 2 Shawwal',
+    '1-shawwal-p2'        => '= 3 Shawwal',
+    '10-dhu-al-hijjah'    => '= 10 Dhu al-Hijjah',
+    '10-dhu-al-hijjah-p1' => '= 11 Dhu al-Hijjah',
+    '10-dhu-al-hijjah-p2' => '= 12 Dhu al-Hijjah',
+    '27-rajab'            => '= 27 Rajab',
+    '1-ramadan'           => '= 1 Ramadan',
+    '12-rabi-al-awwal'    => '= 12 Rabi al-awwal',
 );
