834812c1ed166983696bdd9206883afcdeb5a3ee
KyleK <kylekatarnls@gmail.com>
2019-04-07 11:05:04 +0200

    Move types in their own directory

--- types.php
+++ types.php
@@ -3,6 +3,5 @@
 include __DIR__.'/vendor/autoload.php';
 include __DIR__.'/src/Types/Generator.php';
 
-$sources = __DIR__.'/src';
 $generator = new \Types\Generator();
-$generator->writeHelpers('Cmixin\BusinessDay', $sources, $sources);
+$generator->writeHelpers('Cmixin\BusinessDay', __DIR__.'/src', __DIR__.'/types');
