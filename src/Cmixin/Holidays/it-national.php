e66ef5c0c9d224c59260df0ede358f2648cee038
Kyle <kylekatarnls@users.noreply.github.com>
2020-03-17 09:12:44 +0100

    Restore trailing coma

--- src/Cmixin/Holidays/it-national.php
+++ src/Cmixin/Holidays/it-national.php
@@ -14,5 +14,5 @@ return [
     '12-08'              => '12-08',
     'christmas'          => '12-25',
     'christmas-next-day' => '12-26',
-    'unification-day'    => '03-17 on year 2011'
+    'unification-day'    => '03-17 on year 2011',
 ];
