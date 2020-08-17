ff9c12d61f496375f07b9b55e852e13b11c5ee2e
Timo Ziegler <timo@ziegler.berlin>
2020-08-17 20:40:05 +0200

    + in progress

--- src/Cmixin/Holidays/de-national.php
+++ src/Cmixin/Holidays/de-national.php
@@ -2,22 +2,13 @@
 
 return [
     'new-year'                => '01-01',
-    '02-14'                   => '02-14',
-    'easter-52'               => '= easter -52',
-    'easter-48'               => '= easter -48',
-    'easter-46'               => '= easter -46',
-    'easter-3'                => '= easter -3',
     'easter-2'                => '= easter -2',
     'easter'                  => '= easter',
     'easter-p1'               => '= easter 1',
     '05-01'                   => '05-01',
-    '2nd-sunday-of-may'       => '= second Sunday of May',
     'easter-39'               => '= easter 39',
-    'easter-49'               => '= easter 49',
     'easter-50'               => '= easter 50',
     '10-03'                   => '10-03',
-    '11-01'                   => '11-01',
-    '11-02'                   => '11-02',
     '11-11'                   => '11-11',
     'wednesday-before-11-23'  => '= Wednesday before 11-23',
     '6th-sunday-before-12-25' => '= Sunday before 11-20',
@@ -30,3 +21,16 @@ return [
     'christmas-next-day'      => '12-26',
     '2017-10-31'              => '= 2017-10-31',
 ];
+
+removed 02-14: Valentinstag: no public holiday in any German state
+removed easter-52: Weiberfastnacht: no public holiday in any German state
+removed easter-48: Rosenmontag: no public holiday in any German state
+removed easter-46: Aschermittwoch: no public holiday in any German state
+removed easter-3: Gründonnerstag: no public holiday in any German state
+removed 2nd-sunday-of-may: Muttertag:  no public holiday in any German state, but anyway always on a sunday
+removed easter-49: Pfingstsonntag: no public holiday in most German states
+removed 11-01: Allerheiligen: no public holiday in most German states
+removed 11-02: no name, no public holiday in any German state
+
+
+https://de.wikipedia.org/wiki/Gesetzliche_Feiertage_in_Deutschland
