12bab9742d11e8deea2195d4008dfd5916ab057a
redcuillin <roland@tannerritchie.com>
2021-02-18 12:09:55 -0500

    Update ca-on.php
    Calculation of Ontario family day was incorrect. It is the 3rd Monday of the month, not the 3rd Monday *after* 1st of the month. As a result for 2021 it calculated the date of Family Day a week late.

--- src/Cmixin/Holidays/ca-on.php
+++ src/Cmixin/Holidays/ca-on.php
@@ -1,6 +1,6 @@
 <?php
 
 return array_replace(require __DIR__.'/ca-national.php', [
-    '3rd-monday-after-02-01' => '= third Monday after 02-01',
+    '3rd-monday-of-02-01' => '= third Monday of 02-01',
     'easter-p1'              => '= easter 1',
 ]);
