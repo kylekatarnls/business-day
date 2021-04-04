06bed9fa2af1857885d0325e09e2eef3667ea451
diverpl <mkubien@gmail.com>
2021-04-05 00:52:08 +0200

    Correct Poland holiday names

--- src/Cmixin/Holidays/pl-national.php
+++ src/Cmixin/Holidays/pl-national.php
@@ -4,7 +4,7 @@ return [
     'new-year'           => '01-01',
     '01-06'              => '01-06',
     'easter'             => '= easter',
-    'easter-p1'          => '= easter 1',
+    'easter-1'           => '= easter 1',
     '05-01'              => '05-01',
     '05-03'              => '05-03',
     // '05-26'              => '05-26',
@@ -12,7 +12,7 @@ return [
     'easter-60'          => '= easter 60',
     '08-15'              => '08-15',
     '11-01'              => '11-01',
-    '11-11'              => '11-11',
+    'independence-day'   => '11-11',
     'christmas'          => '12-25',
     'christmas-next-day' => '12-26',
 ];
