96fb134c0ad441afa41ba91cc8c9358b846fd3cb
kylekatarnls <kylekatarnls@gmail.com>
2021-06-15 14:48:54 +0200

    Disable second Monday of June in AU QLD and WA.

--- src/Cmixin/Holidays/au-wa.php
+++ src/Cmixin/Holidays/au-wa.php
@@ -4,6 +4,6 @@ return array_replace(require __DIR__.'/au-national.php', [
     '1st-monday-of-march' => '= first Monday of March',
     'easter-1'            => '= easter -1',
     '1st-monday-of-june'  => '= first Monday of June',
-    '2nd-monday-of-june'  => '= second Monday of June',
+    '2nd-monday-of-june'  => null,
     'monday-before-10-01' => '= Monday before 10-01',
 ]);
---
+++ tests/Cmixin/Holidays/AuNswTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Cmixin\Holidays;
+
+use Cmixin\BusinessDay;
+use PHPUnit\Framework\TestCase;
+
+class AuNswTest extends TestCase
+{
+    const CARBON_CLASS = 'Carbon\Carbon';
+
+    protected function setUp(): void
+    {
+        BusinessDay::enable(static::CARBON_CLASS);
+        $carbon = static::CARBON_CLASS;
+        $carbon::resetHolidays();
+    }
+
+    public function testHolidays()
+    {
+        $carbon = static::CARBON_CLASS;
+        $carbon::resetHolidays();
+        $carbon::setHolidaysRegion('au-nsw');
+
+        self::assertTrue($carbon::parse('2021-06-14')->isHoliday());
+    }
+}
