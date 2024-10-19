1b682e75cb2fc5255bd8a5538ad67061169807a9
sotten <soenio93@gmail.com>
2024-10-19 14:03:28 +0200

    Add German One-time public holiday
    One-time public holiday in all states, including those not normally observing Reformation Day, to mark the 500th anniversary of the Reformation in 2017.
    
    Source: https://en.wikipedia.org/wiki/Public_holidays_in_Germany#cite_ref-10

--- src/Cmixin/Holidays/de-national.php
+++ src/Cmixin/Holidays/de-national.php
@@ -11,4 +11,5 @@ return [
     'german-unity-day'        => '10-03',
     'christmas'               => '12-25',
     'christmas-next-day'      => '12-26',
+    'reformation-day'         => '2017-10-31',
 ];
