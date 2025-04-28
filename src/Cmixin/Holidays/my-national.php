cd14ccb3b027509656e265794f5e37fe8d884ba0
ziming <ziming@users.noreply.github.com>
2025-04-29 03:18:55 +0800

    remove new year and add malaysia day

--- src/Cmixin/Holidays/my-national.php
+++ src/Cmixin/Holidays/my-national.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'new-year'                                     => '01-01',
-    'substitutes-01-01-if-sunday-then-next-monday' => '= 01-01 if sunday then next monday',
     'labor-day'                                    => '05-01',
     'substitutes-05-01-if-sunday-then-next-monday' => '= 05-01 if sunday then next monday',
     'merdeka-day'                                  => '08-31',
     'substitutes-08-31-if-sunday-then-next-monday' => '= 08-31 if sunday then next monday',
+    'malaysia-day'                                 => '09-16',
+    'substitutes-09-16-if-sunday-then-next-monday' => '= 09-16 if sunday then next monday',
     'christmas'                                    => '12-25',
     'substitutes-12-25-if-sunday-then-next-monday' => '= 12-25 if sunday then next monday',
 ];
