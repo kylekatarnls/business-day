7164d2a4eef9b7748ae438cbad6e03a5e3864429
eugenia <eugebib@gmail.com>
2025-05-01 17:54:32 -0300

    Update Argentina holidays for 2025 Remove outdated translations/substitutes and fix translations for overlapping holidays

--- src/Cmixin/Holidays/ar-national.php
+++ src/Cmixin/Holidays/ar-national.php
@@ -4,29 +4,22 @@ return [
     'new-year'                                           => '01-01',
     'easter-48'                                          => '= easter -48',
     'easter-47'                                          => '= easter -47',
-    '03-24'                                              => '03-24',
-    'substitutes-03-24'                                  => '= 03-24 if Tuesday then previous Monday and if Thursday then next Friday',
+    'day-of-remembrance-for-truth-and-justice'           => '03-24',
     'easter-3'                                           => '= easter -3',
     'easter-2'                                           => '= easter -2',
     'day-of-the-veterans-and-the-fallen-of-malvinas-war' => '04-02',
-    'substitutes-04-02'                                  => '= 04-02 if Tuesday then previous Monday and if Thursday then next Friday',
-    '05-01'                                              => '05-01',
-    'substitutes-05-01'                                  => '= 05-01 if Tuesday then previous Monday and if Thursday then next Friday',
-    '05-25'                                              => '05-25',
-    'substitutes-05-25'                                  => '= 05-25 if Tuesday then previous Monday and if Thursday then next Friday',
+    'labour-day-argentina'                               => '05-01',
+    'first-national-government'                          => '05-25',
     'guemes-day'                                         => '= 06-17 if Tuesday,Wednesday then previous Monday and if Thursday,Friday then next Monday',
-    '06-20'                                              => '06-20',
-    'substitutes-06-20'                                  => '= 06-20 if Tuesday then previous Monday and if Thursday then next Friday',
-    '07-09'                                              => '07-09',
-    'jose-de-san-martin'                                 => '= 08-17 if Tuesday,Wednesday then previous Monday and if Thursday,Friday then next Monday',
+    'belgrano-day'                                       => '06-20',
+    'independence-day-argentina'                         => '07-09',
+    'san-martin-day'                                     => '= 08-17 if Tuesday,Wednesday then previous Monday and if Thursday,Friday then next Monday',
     'day-of-respect-for-cultural-diversity'              => '= 10-12 if Tuesday,Wednesday then previous Monday and if Thursday,Friday then next Monday',
-    'substitutes-day-of-respect-for-cultural-diversity'  => '2021-10-08',    //especial day for 2021
-    'day-of-national-sovereignty'                        => '11-20',
-    'substitutes-soberany-day'                           => '= 11-20 if Tuesday,Wednesday then previous Monday and if Thursday,Friday then next Monday',
-    'substitutes-soberany-day-2021'                      => '2021-11-22',    //especial day for 2021
-    '12-08'                                              => '12-08',
-    'substitutes-12-08'                                  => '= 12-08 if Tuesday then previous Monday and if Thursday then next Friday',
-    'substitutes-12-24'                                  => '= 12-24 if Tuesday then previous Monday',
+    'day-of-national-sovereignty'                        => '= 11-20 if Tuesday,Wednesday then previous Monday and if Thursday,Friday then next Monday',
+    'feast-of-the-immaculate-conception'                 => '12-08',
     'christmas'                                          => '12-25',
-    'substitutes-12-25'                                  => '= 12-25 if Thursday then next Friday',
+    'substitutes-soberany-day-2021'                      => '2021-11-22',    // special day for 2021
+    'bridge-holiday-2025-05-02'                          => '2025-05-02',    // special day for 2025
+    'bridge-holiday-2025-08-15'                          => '2025-08-15',    // special day for 2025
+    'bridge-holiday-2025-11-21'                          => '2025-11-21',    // special day for 2025
 ];
