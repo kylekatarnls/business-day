311d876f1af021f445d306b3b59e65ba3e3856f4
Matthew Smith <msmith@accm.edu.au>
2025-06-27 16:08:07 +1000

    Remove 12-24 as national holiday

--- src/Cmixin/Holidays/au-nt.php
+++ src/Cmixin/Holidays/au-nt.php
@@ -3,4 +3,5 @@
 return array_replace(require __DIR__.'/au-national.php', [
     '1st-monday-of-may'    => '= first Monday of May',
     '1st-monday-of-august' => '= first Monday of August',
+    '12-24-19:00'          => '= 12-24 19:00',
 ]);
--- src/Cmixin/Holidays/au-qld.php
+++ src/Cmixin/Holidays/au-qld.php
@@ -4,4 +4,5 @@ return array_replace(require __DIR__.'/au-national.php', [
     '1st-monday-of-october' => '= first Monday of October',
     '1st-monday-of-may'     => '= first Monday of May',
     '2nd-monday-of-june'    => null,
+    '12-24-18:00'           => '= 12-24 18:00',
 ]);
